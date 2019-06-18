61c580cbbd546dd84bc602bad30cbae28d5351e0
java-chip <takuyam0111@yahoo.co.jp>
2019-06-18 23:07:51 +0900

    cifar10を識別できるようにした

--- CapsNet-Keras/capsulenet.py
+++ CapsNet-Keras/capsulenet.py
@@ -203,6 +203,15 @@ def load_mnist():
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
 
+def load_cifar10():
+    from keras.datasets import cifar10
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train = x_train.reshape(-1, 32, 32, 3).astype('float32') / 255.
+    x_test = x_test.reshape(-1, 32, 32, 3).astype('float32') / 255.
+    y_train = to_categorical(y_train.astype('float32'))
+    y_test = to_categorical(y_test.astype('float32'))
+    return (x_train, y_train), (x_test, y_test)
+
 
 if __name__ == "__main__":
     import os
@@ -211,7 +220,8 @@ if __name__ == "__main__":
     from keras import callbacks
 
     # setting the hyper parameters
-    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
+    parser = argparse.ArgumentParser(description="Capsule Network.")
+    parser.add_argument('--dataset', default="mnist", type=str)
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--batch_size', default=100, type=int)
     parser.add_argument('--lr', default=0.001, type=float,
@@ -240,7 +250,10 @@ if __name__ == "__main__":
         os.makedirs(args.save_dir)
 
     # load data
-    (x_train, y_train), (x_test, y_test) = load_mnist()
+    if args.dataset == "mnist":
+        (x_train, y_train), (x_test, y_test) = load_mnist()
+    elif args.dataset == "cifar10":
+        (x_train, y_train), (x_test, y_test) = load_cifar10()
 
     # define model
     model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
@@ -258,3 +271,4 @@ if __name__ == "__main__":
             print('No weights are provided. Will test using random initialized weights.')
         manipulate_latent(manipulate_model, (x_test, y_test), args)
         test(model=eval_model, data=(x_test, y_test), args=args)
+
